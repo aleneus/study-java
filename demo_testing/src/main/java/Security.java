d9cb29b926c476e71832db525690f4186c6504e6
aleneus <aleneus@gmail.com>
2020-05-20 22:18:07 +0300

    feat: stubby add and check user

--- demo_testing/src/main/java/Security.java
+++ demo_testing/src/main/java/Security.java
@@ -16,4 +16,28 @@ public class Security {
     public Security(Storage s) {
 	storage = s;
     }
+
+    /**
+     * Registers new user
+     */
+    public boolean addUser(String name, String passwd) {
+	String s = storage.read();
+	s = s + buildLine(name, passwd) + "\n";
+	storage.write(s);
+	return true;
+    }
+
+    /**
+     * Checks if user exists
+     */
+    public boolean userExists(String name) {
+	return false;
+    }
+
+    /**
+     * Builds line with record about single user
+     */
+    public String buildLine(String name, String passwd) {
+	return name + ":" + passwd;
+    }
 }
--- demo_testing/src/test/java/SecurityTest.java
+++ demo_testing/src/test/java/SecurityTest.java
@@ -4,8 +4,42 @@ import security.*;
 
 
 public class SecurityTest {
-    @Test public void testNew() {
-	Security s = new Security(new StubStorage());
-	assertTrue(s != null);
+    @Test public void testAddUser_ContentIncreased() {
+	StubStorage st = new StubStorage();
+	Security sc = new Security(st);
+	sc.addUser("user", "password");
+	String content = st.read();
+	assertTrue(content.length() > 0);
+    }
+
+    @Test public void testAddUser_ProperContent() {
+	StubStorage st = new StubStorage();
+	Security sc = new Security(st);
+	sc.addUser("user", "password");
+	String content = st.read();
+	assertEquals(content, "user:password\n");
+    }
+
+    @Test public void testAddUser_TwoDifferentUsers() {
+	StubStorage st = new StubStorage();
+	Security sc = new Security(st);
+	sc.addUser("user1", "password1");
+	sc.addUser("user2", "password2");
+	String content = st.read();
+	assertEquals(content, "user1:password1\nuser2:password2\n");
+    }
+
+    @Test public void testAddUserOutput_SingleUser() {
+	StubStorage st = new StubStorage();
+	Security sc = new Security(st);
+	boolean added = sc.addUser("us", "ps");
+	assertTrue(added);
+    }
+
+    @Test public void testUserExists_NoUsers() {
+	StubStorage st = new StubStorage();
+	Security sc = new Security(st);
+	boolean e = sc.userExists("us");
+	assertFalse(e);
     }
 }
